890ad9b0f57fca90cd553c391cacef83719f7a9c
Sayan Dey <roboticsayan@outlook.com>
2022-06-04 17:18:15 +0530

    fix layout

--- src/components/Coming.jsx
+++ src/components/Coming.jsx
@@ -14,24 +14,26 @@ const data = [
 
 function Coming() {
     return (
-        <div className="wrapper">
-            <h1>coming soon<span className="dot">.</span></h1>
-            <div className="link__div">
-                <Link to="/about" className="page__link">sayandey.dev</Link>
+        <div className="container home__container">
+            <div>
+                <h1>coming soon<span className="dot">.</span></h1>
+                <div className="link__div">
+                    <Link to="/about" className="page__link">sayandey.dev</Link>
+                </div>
+
+                {
+                    data.map(({ facebook, instagram, email }, index) => {
+                        return (
+                            <div className="icons">
+                                <a href={facebook} target="_blank"><FaFacebook /></a>
+                                <a href={instagram} target="_blank"><FaInstagram /></a>
+                                <a href={email} target="_blank"><FaPaperPlane /></a>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
-
-            {
-                data.map(({ facebook, instagram, email }, index) => {
-                    return (
-                        <div className="icons">
-                            <a href={facebook} target="_blank"><FaFacebook /></a>
-                            <a href={instagram} target="_blank"><FaInstagram /></a>
-                            <a href={email} target="_blank"><FaPaperPlane /></a>
-                        </div>
-                    )
-                })
-            }
-
         </div>
     )
 }
